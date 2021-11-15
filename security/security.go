6fb01a8a37e82a7db68b99a3761f33ccfa7930dc
Ertan Metin <ertan.metin@daimler.com>
2021-11-15 10:34:48 +0100

    initial version

--- security/security.go
+++ security/security.go
@@ -10,38 +10,11 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
 	errmgmt "github.com/EEAM/gohelplib/errormanagement"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/google/uuid"
 )
-
-func AquireTokenUrlEncoded(endpointUrl string, queryString url.Values) (string, error) {
-
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", endpointUrl, strings.NewReader(queryString.Encode())) // URL-encoded payload
-	if err != nil {
-		return "", fmt.Errorf("error for creating http.Request for the endpoint: %v and url encoded parameter:\n%v", endpointUrl, queryString.Encode())
-	}
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Content-Length", strconv.Itoa(len(queryString.Encode())))
-	resp, err := client.Do(req)
-
-	if err != nil {
-		return "", fmt.Errorf("error for creating http.Request for the endpoint: %v and url encoded parameter:\n%v", endpointUrl, queryString.Encode())
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	bodyS := string(body)
-	log.Println(string(body))
-
-	if err != nil && resp.StatusCode == 200 {
-		return "", errmgmt.ErrorAccessTokenInvalid{Url: endpointUrl, Code: resp.StatusCode, Message: bodyS}
-	}
-
-	return bodyS, nil
-}
 
 type Maker interface {
 	CreateToken(username string, duration time.Duration) (string, error)
@@ -133,4 +106,32 @@ func NewPayload(username string, duration time.Duration) (*Payload, error) {
 		ExpiredAt: time.Now().Add(duration),
 	}
 	return payload, nil
+}
+
+func AquireTokenUrlEncoded(endpointUrl string, queryString url.Values) (string, error) {
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", endpointUrl, strings.NewReader(queryString.Encode())) // URL-encoded payload
+	if err != nil {
+		return "", fmt.Errorf("error for creating http.Request for the endpoint: %v and url encoded parameter:\n%v", endpointUrl, queryString.Encode())
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(queryString.Encode())))
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", fmt.Errorf("error for creating http.Request for the endpoint: %v and url encoded parameter:\n%v", endpointUrl, queryString.Encode())
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	bodyS := string(body)
+	log.Println(string(body))
+
+	if err != nil && resp.StatusCode == 200 {
+		return "", errmgmt.ErrorAccessTokenInvalid{Url: endpointUrl, Code: resp.StatusCode, Message: bodyS}
+	}
+
+	return bodyS, nil
 }
